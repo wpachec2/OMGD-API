f83d4934c8143722646dd53ef1a7d12539b997ce
wpachec2 <fredojpacheco@gmail.com>
2023-02-15 10:56:11 -0500

    finished

---
+++ OMGD API/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OMGD_API.Models;
+
+namespace OMGD_API.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult MovieSearch() //From Form in MovieSearch view
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult MovieSearch(string Title) //Sends back to MovieSearch view
+    {
+        return View(MovieDAL.GetMovie(Title));
+    }
+
+
+    [HttpGet]
+    public IActionResult MovieNight() //From Form in MovieNight view
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult MovieNight(string Title1, string Title2, string Title3) //Sends back to MovieNight view
+    {
+        List<Movie> result = new List<Movie>().ToList();
+
+        result.Add(MovieDAL.GetMovie(Title1));
+        result.Add(MovieDAL.GetMovie(Title2));
+        result.Add(MovieDAL.GetMovie(Title3));
+
+        return View(result);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ OMGD API/Models/MovieDAL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace OMGD_API.Models
+{
+	public class MovieDAL
+	{
+        public static Movie GetMovie(string input)
+        {
+            //Setup
+            string url = $"https://www.omdbapi.com/?t={input}&apikey=f7c39b2c";
+
+            //Request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Convert it to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //Convert to C#
+            Movie result = JsonConvert.DeserializeObject<Movie>(JSON);
+            return result;
+        }
+    }
+}
+
